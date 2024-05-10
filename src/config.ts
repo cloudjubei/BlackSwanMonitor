344d891a14d86a332c3f705ab10e77eab06d6434
Kamil Chmurzynski <cloudjubei@gmail.com>
2024-05-11 01:09:05 +0200

    Config update

--- src/config.ts
+++ src/config.ts
@@ -10,44 +10,45 @@ export const INTERVALS : string[] = [
 ]
 export const TOKENS : string[] = [
     'BTCFDUSD',
-    'XRPFDUSD',
-    'ETHFDUSD',
-    'DOTFDUSD',
-    'DOGEFDUSD',
-    'SOLFDUSD',
-    'LTCFDUSD'
+    'BTCUSDT',
+    'BTCTUSD',
+    'BTCUSDC',
+    'ETHUSDT',
+    'ETHUSDC',
+    'XRPUSDT',
+    'DOTUSDT',
+    'DOGEUSDT',
+    'SOLUSDT',
+    'LTCUSDT'
 ]
 export const CONFIG_PRICES : {[token:string] : ConfigConnectionInputModel } = {
     'BTCFDUSD': { "host": "http://localhost", "port": 3002 },
-    'XRPFDUSD': { "host": "http://localhost", "port": 3002 },
-    'ETHFDUSD': { "host": "http://localhost", "port": 3002 },
-    'DOTFDUSD': { "host": "http://localhost", "port": 3002 },
-    'DOGEFDUSD': { "host": "http://localhost", "port": 3002 },
-    'SOLFDUSD': { "host": "http://localhost", "port": 3002 },
-    'LTCFDUSD': { "host": "http://localhost", "port": 3002 },
+    'BTCUSDT': { "host": "http://localhost", "port": 3002 },
+    'BTCTUSD': { "host": "http://localhost", "port": 3002 },
+    'BTCUSDC': { "host": "http://localhost", "port": 3002 },
+    'ETHUSDT': { "host": "http://localhost", "port": 3002 },
+    'ETHUSDC': { "host": "http://localhost", "port": 3002 },
+    'XRPUSDT': { "host": "http://localhost", "port": 3002 },
+    'DOTUSDT': { "host": "http://localhost", "port": 3002 },
+    'DOGEUSDT': { "host": "http://localhost", "port": 3002 },
+    'SOLUSDT': { "host": "http://localhost", "port": 3002 },
+    'LTCUSDT': { "host": "http://localhost", "port": 3002 },
 }
 export const INDICATORS : string[] = [
-    'bollinger20High',
-    'bollinger20Mid',
-    'bollinger20Low',
-    'rsi9',
-    'rsi14',
-    'rsi30',
-    'ema20',
-    'ema50',
-    'ema200',
-    'macdSignal9',
-    'macd9',
-    'williams14',
-    'williams30',
-    "dump1", 
-    "dump3", 
-    "dump5",
-    "dump10",
-    "pump1", 
-    "pump3", 
-    "pump5",
-    "pump10"
+    "atr5", "atr8", "atr13", "atr30",
+    "rsi9", "rsi11", "rsi14", "rsi20", "rsi30", 
+    "williams14", "williams30", 
+    "wlti",
+    "ema12", "ema20", "ema26", "ema30", 
+    "sar001_01", "sar002_02", "sar005_03",
+    "macdLine", "macdSignal9", "macd9", 
+    "bollinger20Mid", "bollinger20SD", "bollinger20High", "bollinger20Low",
+    "kallman5", "kallman14", "kallman20", "kallman30",
+    "lwti8", "lwti13", "lwti30",
+    "donchianChannels14", "donchianChannels20", "donchianChannels96",
+    "dump1", "dump3", "dump5", "dump10",
+    "pump1", "pump3", "pump5", "pump10",
+    "lwStrategyUp", "lwStrategyDown"
 ]
 
 export const CONFIG_SIGNALS : {[signal:string] : ConfigConnectionInputModel } = {
@@ -58,6 +59,7 @@ export const SIGNALS : string[] = [
     "bollingerLowSignal", "bollingerHighWithRSI30Overbought", "bollingerLowWithRSI30Oversold", 
     "rsi30Overbought", "rsi30Oversold", "rsi9Overbought", "rsi9Oversold",
     "dump1_00_01", "pump1_00_01", "dump3_00_02", "pump3_00_02", "dump5_00_05", "pump5_00_05", "dump10_00_10", "pump10_00_10", 
+    "lwStrategyUpSignal", "lwStrategyDownSignal", 
 ]
 export const SIGNAL_NAMES : { [signal:string]: string } = {
     "bollingerHighWithRSI30Overbought" : "Core Sell Signal",
@@ -75,5 +77,7 @@ export const SIGNAL_NAMES : { [signal:string]: string } = {
     "dump5_00_05" : "Dump 5candles <0.05%",
     "pump5_00_05" : "Pump 5candles >0.05%",
     "dump10_00_10" : "Dump 5candles <0.1%",
-    "pump10_00_10" : "Pump 5candles >0.1%"
+    "pump10_00_10" : "Pump 5candles >0.1%",
+    "lwStrategyUpSignal": "Larry Williams Buy Strategy Indicator",
+    "lwStrategyDownSignal": "Larry Williams Sell Strategy Indicator"
 }
